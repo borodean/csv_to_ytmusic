c2657e975cad60029f57453c2d729ea9622ece21
Vadym Borodin <borodean@gmail.com>
2024-06-05 04:45:16 +0300

    Setup only Youtube

--- spotify_to_ytmusic/setup.py
+++ spotify_to_ytmusic/setup.py
@@ -17,35 +17,12 @@ def setup(file: Optional[Path] = None):
 
     if not DEFAULT_PATH.is_file():
         shutil.copy(EXAMPLE_PATH, DEFAULT_PATH)
-    choice = input("Choose which API to set up\n" "(1) Spotify\n" "(2) YouTube\n" "(3) both\n")
-    choices = ["1", "2", "3"]
-    if choice not in choices:
-        sys.exit("Invalid choice")
 
-    if choice == choices[0]:
-        setup_spotify()
-    elif choice == choices[1]:
-        setup_youtube()
-    elif choice == choices[2]:
-        setup_spotify()
-        setup_youtube()
+    setup_youtube()
 
 
 def setup_youtube():
     settings = Settings()
     credentials = ytmusicapi.setup_oauth(open_browser=has_browser())
     settings["youtube"]["headers"] = json.dumps(credentials.as_dict())
-    settings.save()
-
-
-def setup_spotify():
-    settings = Settings()
-    credentials = {
-        "client_id": input("Paste your client id from the Spotify developer dashboard:"),
-        "client_secret": input("Paste your client secret from the Spotify developer dashboard:"),
-        "use_oauth": input(
-            "Use OAuth method for authorization to transfer private playlists (yes/no):"
-        ),
-    }
-    settings["spotify"].update(credentials)
     settings.save()
